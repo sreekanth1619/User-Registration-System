19e6fe1309ddf5205ddba4b13226532f1a39d3d2
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-14 16:58:33 +0530

    Added: validating the password according to rule 2

--- userregistrationsystem/UserRegistrationMain.java
+++ userregistrationsystem/UserRegistrationMain.java
@@ -19,6 +19,7 @@ public class UserRegistrationMain {
 	        System.out.println("Enter 3 : to validate E-mail Address ");
 	        System.out.println("Enter 4 : to validate Mobile Number ");
 	        System.out.println("Enter 5 : to validate Password Rule 1 ");
+	        System.out.println("Enter 6 : to validate Password Rule 2 ");
 	        switch (scanner.nextInt()){
 	            case 1:
 	                UserRegistration.validFirstName();
@@ -34,7 +35,9 @@ public class UserRegistrationMain {
 	                break;
 	            case 5:
 	                UserRegistration.validPassRule1();
-	                break;
+	            case 6:
+	            	UserRegistration.validPassRule2();
+	            	break;
 	            default:
 	                System.out.println("Select a valid number");
 	        }
