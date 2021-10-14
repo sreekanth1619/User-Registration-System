a1fe903e8ae785ed66ee2aa1428343e248472014
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-14 16:07:11 +0530

    validating the first name

---
+++ userregistrationsystem/UserRegistrationMain.java
@@ -0,0 +1,25 @@
+package userregistrationsystem;
+import java.util.Scanner;
+/**
+ * Purpose  - As a User need to enter a valid First Name
+ * @author  - N sreekanth
+ * @version - 16.0
+ * 
+ */
+public class UserRegistrationMain {
+	public static void welcome(){
+        System.out.println("Welcome to User Registration System Problem");
+    }
+    public static void main(String[] args) {
+	    welcome();
+        UserRegistration user = new UserRegistration();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter 1 : to validate First Name ");
+        switch (scanner.nextInt()){
+            case 1:
+                UserRegistration.validFirstName();
+                break;
+        }
+    }
+}
