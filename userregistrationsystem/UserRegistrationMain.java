8e17d395f10e449995b8fd6d0415cbb92ef14a3a
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-14 16:50:02 +0530

    Added: validating the password_1

--- userregistrationsystem/UserRegistrationMain.java
+++ userregistrationsystem/UserRegistrationMain.java
@@ -17,6 +17,8 @@ public class UserRegistrationMain {
 	        System.out.println("Enter 1 : to validate First Name ");
 	        System.out.println("Enter 2 : to validate Last Name ");
 	        System.out.println("Enter 3 : to validate E-mail Address ");
+	        System.out.println("Enter 4 : to validate Mobile Number ");
+	        System.out.println("Enter 5 : to validate Password Rule 1 ");
 	        switch (scanner.nextInt()){
 	            case 1:
 	                UserRegistration.validFirstName();
@@ -27,8 +29,15 @@ public class UserRegistrationMain {
 	            case 3:
 	                UserRegistration.validEmailId();
 	                break;
+	            case 4:
+	                UserRegistration.validMobileNumber();
+	                break;
+	            case 5:
+	                UserRegistration.validPassRule1();
+	                break;
 	            default:
 	                System.out.println("Select a valid number");
 	        }
 	    }
 }
+
