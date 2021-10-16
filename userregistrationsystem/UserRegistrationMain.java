c8226866aa811ecb67510e9b580d3f10ad30ba4b
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-16 15:13:42 +0530

    Added: validating the email adress

--- userregistrationsystem/UserRegistrationMain.java
+++ userregistrationsystem/UserRegistrationMain.java
@@ -17,7 +17,7 @@ public class UserRegistrationMain {
 	        System.out.println("Enter 1 : to validate First Name ");
 	        System.out.println("Enter 2 : to validate Last Name ");
 	        System.out.println("Enter 3 : to validate E-mail Address ");
-	        System.out.println("Enter 4 : to validate Mobile Number ");
+	        
 	       
 	        switch (scanner.nextInt()){
 	            case 1:
@@ -29,10 +29,7 @@ public class UserRegistrationMain {
 	            case 3:
 	                UserRegistration.validEmailId();
 	                break;
-	            case 4:
-	                UserRegistration.validMobileNumber();
-	                break;
-	           
+	          
 	            default:
 	                System.out.println("Select a valid number");
 	        }
